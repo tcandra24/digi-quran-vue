6d35f24cb25ec81a61cb16b3751d54564d7dcd72
tcandra24 <tcandra007@gmail.com>
2025-08-27 11:06:30 +0700

    Change main.ts for PWA

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 
 import App from "@/App.vue";
 import { registerPlugins } from "@core/utils/plugins";
-
+import { useRegisterSW } from "virtual:pwa-register/vue";
 // Styles
 import "@core/scss/template/index.scss";
 import "@layouts/styles/index.scss";
@@ -10,6 +10,17 @@ import "@/assets/styles/styles.scss";
 
 // Create vue app
 const app = createApp(App);
+
+const intervalMS = 60 * 60 * 1000;
+
+useRegisterSW({
+  onRegistered(r) {
+    r &&
+      setInterval(() => {
+        r.update();
+      }, intervalMS);
+  },
+});
 
 // Register plugins
 registerPlugins(app);
